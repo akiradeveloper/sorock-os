5aa9887466820516a87981750267665ac8e6c977
Akira Hayakawa <ruby.wktk@gmail.com>
2022-04-01 23:22:45 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- sorock-core/src/piece_store/sqlite.rs
+++ sorock-core/src/piece_store/sqlite.rs
@@ -1,6 +1,43 @@
 use crate::*;
+use sqlx::sqlite::SqliteConnectOptions;
+use sqlx::sqlite::SqlitePool;
+use sqlx::ConnectOptions;
+use sqlx::Executor;
 use std::path::PathBuf;
-enum Store {
+use std::str::FromStr;
+
+pub enum StoreType {
     Memory,
-    Directory { path: PathBuf },
+    // If the root_dir is empty then a new db is created.
+    Directory { root_dir: PathBuf },
+}
+pub struct State {
+    db_pool: SqlitePool,
+}
+impl State {
+    pub async fn create(typ: StoreType) -> Self {
+        let options = match typ {
+            StoreType::Memory => {
+                let mut options = SqliteConnectOptions::from_str(":memory").unwrap();
+                options
+            }
+            StoreType::Directory { root_dir } => {
+                let mut path = root_dir;
+                path.set_file_name("store.db");
+
+                let mut options =
+                    SqliteConnectOptions::from_str(&format!("sqlite://{}", path.to_str().unwrap()))
+                        .unwrap()
+                        .create_if_missing(true);
+                options
+            }
+        };
+
+        let db_pool = SqlitePool::connect_with(options).await.unwrap();
+
+        let q = include_str!("./sqlite_schema.sql");
+        db_pool.execute(q).await.unwrap();
+
+        Self { db_pool }
+    }
 }
