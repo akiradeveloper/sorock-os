7032d3692ff5da7ff21aaf894a231f613f00a67f
Akira Hayakawa <ruby.wktk@gmail.com>
2022-04-09 11:20:10 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- sorock/src/fd_app_out.rs
+++ sorock/src/fd_app_out.rs
@@ -1,16 +1,14 @@
 use failure_detector as FD;
-use FD::app_out as M;
 use std::sync::Arc;
 use tonic::transport::{Channel, Uri};
+use FD::app_out as M;
 
-pub fn spawn(
-	state: State,
-) -> M::ClientT {
+pub fn spawn(state: State) -> M::ClientT {
     use norpc::runtime::send::*;
     let (tx, rx) = tokio::sync::mpsc::channel(100);
     tokio::spawn(async {
         let svc = App {
-			state: state.into(),
+            state: state.into(),
         };
         let service = M::AppOutService::new(svc);
         let server = ServerExecutor::new(rx, service);
@@ -21,33 +19,36 @@ pub fn spawn(
 }
 
 pub struct State {
-	chan: Channel,
+    chan: Channel,
 }
 impl State {
-	pub fn new(uri: Uri) -> Self {
-		let e = tonic::transport::Endpoint::new(uri).unwrap();
+    pub fn new(uri: Uri) -> Self {
+        let e = tonic::transport::Endpoint::new(uri).unwrap();
         let chan = e.connect_lazy();
-		Self {
-			chan, 
-		}
-	}
+        Self { chan }
+    }
 }
 
 #[derive(Clone)]
 struct App {
-	state: Arc<State>,
+    state: Arc<State>,
 }
 
 #[norpc::async_trait]
 impl M::AppOut for App {
-	async fn notify_failure(mut self, culprit: Uri) {
-		let mut cli1 = sorock_core::proto_compiled::sorock_client::SorockClient::new(self.state.chan.clone());
-		cli1.remove_node(sorock_core::proto_compiled::RemoveNodeReq {
-			uri: culprit.to_string(),
-		}).await.unwrap();
-		let mut cli2 = lol_core::RaftClient::new(self.state.chan.clone());
-		cli2.remove_server(lol_core::api::RemoveServerReq {
-			id: culprit.to_string(),
-		}).await.unwrap();
-	}
+    async fn notify_failure(mut self, culprit: Uri) {
+        let mut cli1 =
+            sorock_core::proto_compiled::sorock_client::SorockClient::new(self.state.chan.clone());
+        cli1.remove_node(sorock_core::proto_compiled::RemoveNodeReq {
+            uri: culprit.to_string(),
+        })
+        .await
+        .unwrap();
+        let mut cli2 = lol_core::RaftClient::new(self.state.chan.clone());
+        cli2.remove_server(lol_core::api::RemoveServerReq {
+            id: culprit.to_string(),
+        })
+        .await
+        .unwrap();
+    }
 }
--- sorock/src/main.rs
+++ sorock/src/main.rs
@@ -79,12 +79,16 @@ async fn main() -> anyhow::Result<()> {
 
     use failure_detector as FD;
     let peer_out_cli = FD::peer_out::spawn(FD::peer_out::State::new());
-    let app_out_cli = todo!();
-    let queue_cli = FD::queue::spawn(peer_out_cli, app_out_cli, FD::queue::State::new());
+    let app_out_cli = fd_app_out::spawn(fd_app_out::State::new(uri.clone()));
+    let queue_cli = FD::queue::spawn(peer_out_cli.clone(), app_out_cli, FD::queue::State::new());
     let reporter_cli =
         FD::reporter::spawn(queue_cli.clone(), FD::reporter::State::new(uri.clone()));
     let mut app_in_cli = FD::app_in::spawn(queue_cli.clone(), reporter_cli.clone());
-    let svc2 = FD::server::make_service(FD::server::Server { peer_out_cli, uri }).await;
+    let svc2 = FD::server::make_service(FD::server::Server {
+        peer_out_cli,
+        uri: uri.clone(),
+    })
+    .await;
     tokio::spawn(async move {
         let mut interval = tokio::time::interval(Duration::from_millis(100));
         let mut reporter_cli = reporter_cli.clone();
@@ -117,7 +121,7 @@ async fn main() -> anyhow::Result<()> {
     let svc3 = lol_core::make_raft_service(
         raft_app,
         lol_core::storage::rocksdb::Storage::open(&SOROCKDB_ROOT.join("raft_log"))?,
-        uri,
+        uri.clone(),
         config,
     )
     .await;
