aec233c6e36b0cce9005e6a3d7559ffaaffd48ef
Akira Hayakawa <ruby.wktk@gmail.com>
2022-04-10 16:46:36 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- integration-tests/src/main.rs
+++ integration-tests/src/main.rs
@@ -1,6 +1,10 @@
-use lol_core::{RaftClient, api::*, Uri};
 use cmd_lib::*;
+use lol_core::{api::*, RaftClient, Uri};
+use sorock_core::proto_compiled::*;
+use std::time::Duration;
 use tonic::transport::Endpoint;
+
+const N: usize = 10;
 
 struct Node {
     dest: Uri,
@@ -19,15 +23,36 @@ async fn main() -> anyhow::Result<()> {
     run_cmd!(docker-compose down -v)?;
     run_cmd!(docker-compose up -d)?;
 
+    // Wait for all nodes to start up.
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
     let ep = Endpoint::new(node_list[0].dest.clone())?;
     let chan = ep.connect_lazy();
 
-    for i in 0..1 {
+    for i in 0..2 {
         let mut cli = RaftClient::new(chan.clone());
         let req = AddServerReq {
             id: node_list[i].id.to_string(),
         };
         cli.add_server(req).await?;
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        let mut cli = sorock_client::SorockClient::new(chan.clone());
+        cli.add_node(AddNodeReq {
+            uri: node_list[i].id.to_string(),
+            cap: 1.,
+        })
+        .await?;
+    }
+
+    for i in 0..N {
+        let mut cli = sorock_client::SorockClient::new(chan.clone());
+        cli.create(CreateReq {
+            key: format!("key-{}", i),
+            data: vec![0; 512].into(),
+        })
+        .await?;
     }
 
     run_cmd!(docker-compose down -v)?;
--- sorock-core/tests/fd_app_in_stub.rs
+++ sorock-core/tests/fd_app_in_stub.rs
@@ -6,17 +6,17 @@ use std::collections::HashSet;
 struct App;
 #[norpc::async_trait]
 impl M::AppIn for App {
-	async fn set_new_cluster(mut self, cluster: HashSet<Uri>) {}
+    async fn set_new_cluster(mut self, cluster: HashSet<Uri>) {}
 }
 pub fn spawn() -> M::ClientT {
-	use norpc::runtime::send::*;
-	let (tx, rx) = tokio::sync::mpsc::channel(100);
-	tokio::spawn(async {
-		let svc = App {};
-		let service = M::AppInService::new(svc);
-		let server = ServerExecutor::new(rx, service);
-		server.serve().await
-	});
-	let chan = ClientService::new(tx);
-	M::AppInClient::new(chan)
+    use norpc::runtime::send::*;
+    let (tx, rx) = tokio::sync::mpsc::channel(100);
+    tokio::spawn(async {
+        let svc = App {};
+        let service = M::AppInService::new(svc);
+        let server = ServerExecutor::new(rx, service);
+        server.serve().await
+    });
+    let chan = ClientService::new(tx);
+    M::AppInClient::new(chan)
 }
